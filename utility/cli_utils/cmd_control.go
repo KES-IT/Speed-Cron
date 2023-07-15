9a3082063d0c1c87adf1e0746629c709b0c8e017
hamster1963 <1410514192@qq.com>
2023-07-15 17:33:37 +0800

    perf: 精简了未用到的单次测速

--- utility/cli_utils/cmd_control.go
+++ utility/cli_utils/cmd_control.go
@@ -17,6 +17,7 @@ type uCliUtils struct{}
 
 var CliUtils = &uCliUtils{}
 
+// CreateSpeedCmd 创建测速命令
 func (s *uCliUtils) CreateSpeedCmd() *exec.Cmd {
 	// 获取Mac地址
 	_, macAddress := net_utils.NetworkInfo.GetMacAddress()
@@ -47,34 +48,3 @@ func (s *uCliUtils) CreateSpeedCmd() *exec.Cmd {
 	return exec.Command("speed_cli/speedCLI/speedtest.exe", "--accept-gdpr", "--accept-license", serverIdCmd,
 		"--progress=yes", "--format=json", "--progress-update-interval=200")
 }
-
-/*// StartSingleSpeedTest
-//
-//	@dc: 启动单次测速
-//	@params:
-//	@response:
-//	@author: Administrator   @date:2023-06-17 10:37:40
-func (u *uCliUtils) StartSingleSpeedTest(initData g.Map) (err error) {
-	err = gres.Export("speed/speed_bin.exe", "speed_cli/")
-	if err != nil {
-		glog.Warning(context.Background(), "导出资源文件时发生错误:", err)
-		return
-	}
-	departmentCmd := "-department=" + gconv.String(initData["department"])
-	nameCmd := "-name=" + gconv.String(initData["name"])
-	glog.Info(context.Background(), "部门:", departmentCmd, "姓名:", nameCmd)
-	cmd := exec.Command("speed_cli/speed/speed_bin.exe", departmentCmd, nameCmd)
-	// 启动命令
-	glog.Notice(context.Background(), "启动测速命令")
-	err = cmd.Start()
-	if err != nil {
-		glog.Warning(context.Background(), "启动命令时发生错误:", err)
-		return
-	}
-	glog.Info(context.Background(), "运行pid为:", cmd.Process.Pid)
-	status, err := cmd.Process.Wait()
-	if status.Exited() {
-		glog.Info(context.Background(), "单次测速执行完成")
-	}
-	return
-}*/
