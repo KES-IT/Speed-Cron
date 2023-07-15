636fe4fe78d33331ae92a5b89441b2a387920601
hamster1963 <1410514192@qq.com>
2023-07-15 17:18:17 +0800

    perf: 精简一些变量初始化

--- utility/cli_utils/cmd_core.go
+++ utility/cli_utils/cmd_core.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gogf/gf/v2/errors/gerror"
 	"github.com/gogf/gf/v2/os/gcache"
 	"github.com/gogf/gf/v2/os/glog"
-	"github.com/gogf/gf/v2/util/gconv"
 	"kes-cron/internal/global/g_consts"
 	"time"
 )
@@ -42,15 +41,15 @@ func (u *uCmdCore) StartSpeedCmd(ctx context.Context, initData *g_consts.InitDat
 	var (
 		scanner             = bufio.NewScanner(stdout)
 		defaultBars         = Bar.InitDefaultBar()
-		uploadNetDataStruct = &NetInfoUploadData{}
+		uploadNetDataStruct = &NetInfoUploadData{
+			Department: initData.Department,
+			StaffName:  initData.Name,
+		}
 	)
-	uploadNetDataStruct.Department = gconv.String(initData.Department)
-	uploadNetDataStruct.StaffName = gconv.String(initData.Name)
+
 	// 持续获取输出
 	for scanner.Scan() {
-		// 获取输出行
-		line := scanner.Bytes()
-		ok, err := CmdProgress.CmdCoreProgress(ctx, string(line), defaultBars, uploadNetDataStruct)
+		ok, err := CmdProgress.CmdCoreProgress(ctx, string(scanner.Bytes()), defaultBars, uploadNetDataStruct)
 		if err != nil {
 			glog.Warning(ctx, "处理命令行输出时发生错误:", err)
 			_ = cmd.Process.Kill()
