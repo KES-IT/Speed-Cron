36b17c75d311fcc554627571bd10a14e75b70eaa
hamster1963 <1410514192@qq.com>
2023-08-07 15:06:09 +0800

    test: 增加后端地址与版本号测试

---
+++ g_test/const_test.go
@@ -0,0 +1,41 @@
+package g_test
+
+import (
+	"context"
+	"flag"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/test/gtest"
+	"net/http"
+	"testing"
+)
+
+var (
+	inputGithubTag = flag.String("tag", "", "get github tag")
+	inputBaseUrl   = flag.String("baseurl", "", "get backend base url")
+)
+
+// 测试是否正确获取到后端地址
+func Test_BackendBaseUrl(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		// 获取命令行参数
+		flag.Parse()
+		// 测试是否正确获取到后端地址
+		baseUrl := *inputBaseUrl
+		t.AssertNE(baseUrl, "")
+		// 测试是否正确获取到后端地址且可访问
+		response, err := g.Client().Get(context.Background(), baseUrl)
+		if err != nil {
+			t.Error(err)
+		}
+		t.AssertEQ(response.StatusCode, http.StatusOK)
+	})
+}
+
+// 测试是否正确获取版本号
+func Test_Version(t *testing.T) {
+	gtest.C(t, func(t *gtest.T) {
+		flag.Parse()
+		githubTag := *inputGithubTag
+		t.AssertNE(githubTag, "")
+	})
+}
