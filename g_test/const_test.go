dc3268a2701359fd0f8d76e1a64e46e074b1a938
hamster1963 <1410514192@qq.com>
2023-08-09 11:16:29 +0800

    refactor: 重构测试初始化参数

--- g_test/const_test.go
+++ g_test/const_test.go
@@ -4,27 +4,47 @@ import (
 	"context"
 	"flag"
 	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcache"
 	"github.com/gogf/gf/v2/test/gtest"
+	"kes-cron/internal/global/g_consts"
+	"kes-cron/internal/global/g_structs"
 	"net/http"
 	"testing"
 )
 
 var (
-	InputGithubTag = flag.String("tag", "", "get github tag")
-	InputBaseUrl   = flag.String("baseurl", "", "get backend base url")
+	InputGithubTagFlag = flag.String("tag", "", "get github tag")
+	InputBaseUrlFlag   = flag.String("baseurl", "", "get backend base url")
 )
+
+var (
+	InitData = &g_structs.InitData{
+		Department:   "GitHub",
+		Name:         "Go-Test",
+		LocalVersion: "",
+	}
+	InputGithubTag = ""
+	InputBaseUrl   = ""
+)
+
+func Test_Init(t *testing.T) {
+	// 获取命令行参数
+	flag.Parse()
+	InputGithubTag = *InputGithubTagFlag
+	InputBaseUrl = *InputBaseUrlFlag
+	InitData.LocalVersion = InputGithubTag
+	g_consts.BaseUrl = InputBaseUrl
+	// 设置更新测试标识缓存
+	_ = gcache.Set(context.Background(), "GitHubTestStatus", true, 0)
+}
 
 // 测试是否正确获取到后端地址
 func Test_BackendBaseUrl(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
-		// 获取命令行参数
-		flag.Parse()
 		// 测试是否正确获取到后端地址
-		baseUrl := *InputBaseUrl
-		g.Dump(baseUrl)
-		t.AssertNE(baseUrl, "")
+		t.AssertNE(InputBaseUrl, "")
 		// 测试是否正确获取到后端地址且可访问
-		response, err := g.Client().Get(context.Background(), baseUrl)
+		response, err := g.Client().Get(context.Background(), InputBaseUrl)
 		if err != nil {
 			t.Error(err)
 		}
@@ -35,9 +55,6 @@ func Test_BackendBaseUrl(t *testing.T) {
 // 测试是否正确获取版本号
 func Test_Version(t *testing.T) {
 	gtest.C(t, func(t *gtest.T) {
-		flag.Parse()
-		githubTag := *InputGithubTag
-		g.Dump(githubTag)
-		t.AssertNE(githubTag, "")
+		t.AssertNE(InputGithubTag, "")
 	})
 }
