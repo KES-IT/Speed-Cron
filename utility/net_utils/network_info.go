52b5288b9017daf7f207e83bfabd8953aa995681
hamster1963 <1410514192@qq.com>
2023-08-13 00:35:55 +0800

    test: 增加测试ip与mac返回

--- utility/net_utils/network_info.go
+++ utility/net_utils/network_info.go
@@ -1,7 +1,9 @@
 package net_utils
 
 import (
+	"context"
 	"fmt"
+	"github.com/gogf/gf/v2/os/gcache"
 	"net"
 )
 
@@ -11,6 +13,13 @@ var NetworkInfo = &uNetworkInfo{}
 
 // GetMacAddress 获取Mac地址
 func (u *uNetworkInfo) GetMacAddress() (internalIP, macAddress string) {
+
+	// GitHub测试状态
+	githubTestStatus := gcache.MustGet(context.Background(), "GitHubTestStatus").Bool()
+	if githubTestStatus {
+		return "172.0.0.1", "f0:2f:74:4f:61:ce"
+	}
+
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		fmt.Println("Error:", err)
